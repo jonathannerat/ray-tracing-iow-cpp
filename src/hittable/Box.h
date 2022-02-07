e9423828dc08f44f20c8400a5301b2c3ed2753b2
Jonathan Teran <jonathan.nerat@gmail.com>
2022-02-07 20:03:29 -0300

    Usar constructores y operadores default en Box.h

--- src/hittable/Box.h
+++ src/hittable/Box.h
@@ -8,34 +8,31 @@
 
 class Box : public std::enable_shared_from_this<Box>, public Hittable {
 public:
-  Point3 cback, cfront;
-  shared_ptr<Material> mat_ptr;
+    Point3 cback, cfront;
+    shared_ptr<Material> mat_ptr;
 
-  Box(Point3 cb, Point3 cf, const shared_ptr<Material>& m);
-  Box(const Box &o): cback(o.cback), cfront(o.cfront), mat_ptr(o.mat_ptr), box(o.box), faces(o.faces) {}
+    Box(Point3 cb, Point3 cf, const shared_ptr<Material> &m);
 
-  Box operator+(const Box &o) const;
-  Box& operator=(Box &o) {
-    cback = o.cback;
-    cfront = o.cfront;
-    mat_ptr = o.mat_ptr;
+    Box(const Box &o) = default;
 
-    return *this;
-  }
+    Box operator+(const Box &o) const;
 
-  bool is_inside(const Point3 &p) const;
+    Box &operator=(const Box &o) = default;
 
-  virtual bool hit(const Ray &r, double t_min, double t_max,
-                   HitRecord &rec) const override;
+    bool is_inside(const Point3 &p) const;
 
-  virtual shared_ptr<const Box> bounding_box() const override;
-  
-  virtual void scale(const Vec3 &s) override;
-  virtual void move(const Vec3 &o) override;
+    bool hit(const Ray &r, double t_min, double t_max,
+             HitRecord &rec) const override;
+
+    shared_ptr<const Box> bounding_box() const override;
+
+    void scale(const Vec3 &s) override;
+
+    void move(const Vec3 &o) override;
 
 private:
-  std::vector<Plane> faces;
-  shared_ptr<Box> box;
+    std::vector<Plane> faces;
+    shared_ptr<Box> box;
 };
 
 #endif // BOX_H
